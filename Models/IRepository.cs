dd156b018b60f220a41c34c6ad99a137bac9cf14
u21487822 <u21487822@tuks.co.za>
2023-06-22 22:59:31 +0200

    first commit

---
+++ Models/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment3_Backend.Models
+{
+    public interface IRepository
+    {
+        Task<bool> SaveChangesAsync();
+        Task<Product[]> GetProductsAsync();
+        Task<ProductType[]> GetProductTypesAsync();
+        Task<Brand[]> GetBrandsAsync();
+
+        void Add<T>(T entity) where T : class;
+    }
+}
